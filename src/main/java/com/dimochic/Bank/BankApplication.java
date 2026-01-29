8359934dadf1fd517629b0db93fcaebce6805cb3
dimoch1c <dimitrypatriarch@gmail.com>
2026-01-29 16:32:50 +0500

    feat: Created Swagger Security Scheme

--- src/main/java/com/dimochic/Bank/BankApplication.java
+++ src/main/java/com/dimochic/Bank/BankApplication.java
@@ -1,9 +1,29 @@
 package com.dimochic.Bank;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Bank Management System API",
+                version = "v0.0.1",
+                description = "This API provides endpoints for managing users"
+        )
+)
+@SecurityScheme(
+        name = "Authentication",
+        description = "JWT authentication token",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
 public class BankApplication {
 
 	public static void main(String[] args) {
